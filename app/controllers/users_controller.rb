2ef9d751a7421fa75f6f99828a5986bebf94aa15
Nikolay G Titov <ngtitov@ncsu.edu>
2019-02-15 22:47:13 -0500

    Migrated Devise into existing Users model and adapted authentication:   - rails generate devise:install -s   - rails migration AddDeviseToUsers   - Updated change_table method   - rails generate devise:views -s   - Customized Sign up and Edit pages   - Updated config/routes.rb to include device for Users   - Updated Controllers, Models, and Views to adapt authentication     system for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  # before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :user_signed_in?, only: [:index, :edit, :update, :destroy]
+  before_action :admin_user, only: [:new, :create, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
@@ -71,4 +73,9 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :role)
     end
+
+  private
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: [:admin, :agent, :customer]
+
+  validates :name, presence: true
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
+            uniqueness: { case_sensitive: false }, presence: true
 end

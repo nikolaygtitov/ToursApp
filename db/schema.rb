99871def359a9e12ac8a31413925c53cda4fb39c
yuhan <ychen239@ncsu.edu>
2019-02-20 00:20:16 -0500

    Merge: Review scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 2019_02_20_052806) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "subject"
+    t.text "content"
+    t.integer "user_id"
+    t.integer "tour_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tour_id"], name: "index_reviews_on_tour_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "tours", force: :cascade do |t|
     t.string "name"
     t.text "description"
